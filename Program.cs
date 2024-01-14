2d1347e108090f0f113c11af4f4a02fb1c76fe88
Brett <opsfox245@gmail.com>
2024-01-13 18:11:54 -0600

    First

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Lexer l = new();
+
+        using (StreamReader reader = new StreamReader("sample.vl")){
+            foreach (Token token in l.Lex(reader))
+            {
+                Console.WriteLine($"type: {token.type} = {token.text}");
+            }
+        }
+    }
+}
